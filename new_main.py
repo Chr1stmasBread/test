0413a720d4456613e9146435cd4810d79f46efb7
Chr1stmasBread <93283740+Chr1stmasBread@users.noreply.github.com>
2024-03-19 21:43:58 +0300

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -5,20 +5,10 @@ import sqlite3
 # Создание экземпляра бота
 bot = telebot.TeleBot(TOKEN)
 
-# Создание соединения с базой данных
-conn = sqlite3.connect('user_data.db')
-cursor = conn.cursor()
-
-# Создание таблицы пользователей, если ее еще нет
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                  user_id INTEGER PRIMARY KEY,
-                  genre TEXT,
-                  gender TEXT,
-                  universe TEXT
-                )''')
-
-# Коммит изменений
-conn.commit()
+# Функция для установки соединения с базой данных
+def get_connection():
+    conn = sqlite3.connect('user_data.db', check_same_thread=False)
+    return conn
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
@@ -38,42 +28,44 @@ def handle_text(message):
     user_id = message.from_user.id
 
     # Получаем текущий шаг пользователя из базы данных
-    cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
-    user_data = cursor.fetchone()
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
+        user_data = cursor.fetchone()
 
-    if user_data:
-        # Обновляем данные пользователя в базе данных в зависимости от текущего шага
-        genre, gender, universe = user_data
+        if user_data:
+            # Обновляем данные пользователя в базе данных в зависимости от текущего шага
+            genre, gender, universe = user_data
 
-        if not genre:
-            cursor.execute('UPDATE users SET genre = ? WHERE user_id = ?', (text, user_id))
+            if not genre:
+                cursor.execute('UPDATE users SET genre = ? WHERE user_id = ?', (text, user_id))
+                conn.commit()
+                bot.send_message(message.chat.id, 'Отлично! Теперь выберите пол главного героя:',
+                                 reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                                                one_time_keyboard=True)
+                                                .add('Мужской', 'Женский'))
+            elif not gender:
+                cursor.execute('UPDATE users SET gender = ? WHERE user_id = ?', (text, user_id))
+                conn.commit()
+                bot.send_message(message.chat.id, 'Отлично! Теперь выберите вселенную:',
+                                 reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                                                one_time_keyboard=True)
+                                                .add('Вселенная 1', 'Вселенная 2'))
+            elif not universe:
+                cursor.execute('UPDATE users SET universe = ? WHERE user_id = ?', (text, user_id))
+                conn.commit()
+                bot.send_message(message.chat.id, 'Отлично! Ваши предпочтения сохранены.')
+            else:
+                bot.send_message(message.chat.id, 'Вы уже выбрали все параметры.')
+
+        else:
+            # В базе данных еще нет записи о пользователе, создаем новую
+            cursor.execute('INSERT INTO users (user_id, genre) VALUES (?, ?)', (user_id, text))
             conn.commit()
             bot.send_message(message.chat.id, 'Отлично! Теперь выберите пол главного героя:',
                              reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
                                                                             one_time_keyboard=True)
                                             .add('Мужской', 'Женский'))
-        elif not gender:
-            cursor.execute('UPDATE users SET gender = ? WHERE user_id = ?', (text, user_id))
-            conn.commit()
-            bot.send_message(message.chat.id, 'Отлично! Теперь выберите вселенную:',
-                             reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
-                                                                            one_time_keyboard=True)
-                                            .add('Вселенная 1', 'Вселенная 2'))
-        elif not universe:
-            cursor.execute('UPDATE users SET universe = ? WHERE user_id = ?', (text, user_id))
-            conn.commit()
-            bot.send_message(message.chat.id, 'Отлично! Ваши предпочтения сохранены.')
-        else:
-            bot.send_message(message.chat.id, 'Вы уже выбрали все параметры.')
-
-    else:
-        # В базе данных еще нет записи о пользователе, создаем новую
-        cursor.execute('INSERT INTO users (user_id, genre) VALUES (?, ?)', (user_id, text))
-        conn.commit()
-        bot.send_message(message.chat.id, 'Отлично! Теперь выберите пол главного героя:',
-                         reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
-                                                                        one_time_keyboard=True)
-                                        .add('Мужской', 'Женский'))
 
 # Запуск бота
 bot.polling()
