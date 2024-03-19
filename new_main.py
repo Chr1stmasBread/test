00b9ccbc66c634faafd42f22a35b6a51b31168eb
Chr1stmasBread <93283740+Chr1stmasBread@users.noreply.github.com>
2024-03-19 21:31:41 +0300

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -2,51 +2,45 @@ import telebot
 import sqlite3
 from TOKEN import *
 
-# Создание соединения с базой данных
-conn = sqlite3.connect('user_data.db')
-cursor = conn.cursor()
-
-# Создание таблицы для хранения данных пользователей
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                    user_id INTEGER PRIMARY KEY,
-                    genre TEXT,
-                    gender TEXT,
-                    universe TEXT
-                )''')
-conn.commit()
-
-# Класс для представления состояния пользователя
-class UserState:
-    def __init__(self, user_id):
-        self.user_id = user_id
-        self.genre = None
-        self.gender = None
-        self.universe = None
-
-# Словарь для хранения текущего состояния пользователей
-user_states = {}
-
 # Создание экземпляра бота
 bot = telebot.TeleBot(TOKEN)
+
+# Создание соединения с базой данных
+conn = sqlite3.connect('user_data.db')
+
+# Создание объекта курсора
+cursor = conn.cursor()
+
+# Создание таблицы пользователей, если ее еще нет
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                  user_id INTEGER PRIMARY KEY,
+                  genre TEXT,
+                  gender TEXT,
+                  universe TEXT
+                )''')
+
+# Коммит изменений и закрытие соединения
+conn.commit()
+conn.close()
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    # Проверяем, есть ли пользователь уже в базе данных
+    # Подключение к базе данных
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+
+    # Проверка, есть ли пользователь в базе данных
     cursor.execute('SELECT * FROM users WHERE user_id = ?', (message.from_user.id,))
     existing_user = cursor.fetchone()
+
+    # Если пользователя нет, добавляем его в базу данных с пустыми значениями
     if not existing_user:
-        # Если пользователя нет, добавляем его в базу данных с пустыми значениями
-        cursor.execute('INSERT INTO users VALUES (?, NULL, NULL, NULL)', (message.from_user.id,))
+        cursor.execute('INSERT INTO users (user_id, genre, gender, universe) VALUES (?, NULL, NULL, NULL)', (message.from_user.id,))
         conn.commit()
-        # Создаем объект состояния для нового пользователя
-        user_states[message.from_user.id] = UserState(message.from_user.id)
-    else:
-        # Если пользователь уже есть, загружаем его данные из базы данных
-        user_states[message.from_user.id] = UserState(message.from_user.id)
-        user_states[message.from_user.id].genre = existing_user[1]
-        user_states[message.from_user.id].gender = existing_user[2]
-        user_states[message.from_user.id].universe = existing_user[3]
+
+    # Закрываем соединение с базой данных
+    conn.close()
 
     # Отправляем приветственное сообщение и кнопки для выбора жанра, пола героя и вселенной
     bot.send_message(message.chat.id, 'Привет! Для начала работы выберите жанр:',
@@ -60,34 +54,75 @@ def start(message):
 # Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
 def handle_text(message):
-    if message.text == 'Фантастика' or message.text == 'Фэнтези' or message.text == 'Детектив' or message.text == 'Романтика':
-        user_states[message.from_user.id].genre = message.text
-        bot.send_message(message.chat.id, 'Выберите пол главного героя:',
-                         reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
-                                                                        one_time_keyboard=True,
-                                                                        keyboard=[
-                                                                            ['Мужской', 'Женский']
-                                                                        ]))
-    elif message.text == 'Мужской' or message.text == 'Женский':
-        user_states[message.from_user.id].gender = message.text
-        bot.send_message(message.chat.id, 'Выберите вселенную:',
-                         reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
-                                                                        one_time_keyboard=True,
-                                                                        keyboard=[
-                                                                            ['Земля', 'Другая планета']
-                                                                        ]))
-    elif message.text == 'Земля' or message.text == 'Другая планета':
-        user_states[message.from_user.id].universe = message.text
-        # После выбора всех параметров можно вызвать функцию для генерации текста или что-то еще
-        bot.send_message(message.chat.id, 'Вы выбрали следующие параметры:\n'
-                                          f'Жанр: {user_states[message.from_user.id].genre}\n'
-                                          f'Пол главного героя: {user_states[message.from_user.id].gender}\n'
-                                          f'Вселенная: {user_states[message.from_user.id].universe}')
-    else:
-        bot.send_message(message.chat.id, 'Пожалуйста, воспользуйтесь кнопками для выбора.')
+    # Подключение к базе данных
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+
+    # Обработка текстового сообщения
+    genre = message.text
+    user_id = message.from_user.id
+
+    # Обновляем жанр пользователя в базе данных
+    cursor.execute('UPDATE users SET genre = ? WHERE user_id = ?', (genre, user_id))
+    conn.commit()
+
+    # Закрываем соединение с базой данных
+    conn.close()
+
+    # Отправляем сообщение с предложением выбрать пол главного героя
+    bot.send_message(message.chat.id, 'Отлично! Теперь выберите пол главного героя:',
+                     reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                                    one_time_keyboard=True,
+                                                                    keyboard=[
+                                                                        ['Мужской', 'Женский']
+                                                                    ]))
+
+# Обработчик текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def handle_text(message):
+    # Подключение к базе данных
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+
+    # Обработка текстового сообщения
+    gender = message.text
+    user_id = message.from_user.id
+
+    # Обновляем пол главного героя в базе данных
+    cursor.execute('UPDATE users SET gender = ? WHERE user_id = ?', (gender, user_id))
+    conn.commit()
+
+    # Закрываем соединение с базой данных
+    conn.close()
+
+    # Отправляем сообщение с предложением выбрать вселенную
+    bot.send_message(message.chat.id, 'Отлично! Теперь выберите вселенную:',
+                     reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                                    one_time_keyboard=True,
+                                                                    keyboard=[
+                                                                        ['Вселенная 1', 'Вселенная 2']
+                                                                    ]))
+
+# Обработчик текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def handle_text(message):
+    # Подключение к базе данных
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+
+    # Обработка текстового сообщения
+    universe = message.text
+    user_id = message.from_user.id
+
+    # Обновляем вселенную в базе данных
+    cursor.execute('UPDATE users SET universe = ? WHERE user_id = ?', (universe, user_id))
+    conn.commit()
+
+    # Закрываем соединение с базой данных
+    conn.close()
+
+    # Отправляем сообщение с подтверждением выбора
+    bot.send_message(message.chat.id, 'Отлично! Ваши предпочтения сохранены.')
 
 # Запуск бота
 bot.polling()
-
-# В конце программы закрываем соединение с базой данных
-conn.close()
