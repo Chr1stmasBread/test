409acc3026ebd773dd9c849d831ff4ba3010689a
Chr1stmasBread <93283740+Chr1stmasBread@users.noreply.github.com>
2024-03-19 21:34:21 +0300

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -1,6 +1,6 @@
+from TOKEN import *
 import telebot
 import sqlite3
-from TOKEN import *
 
 # Создание экземпляра бота
 bot = telebot.TeleBot(TOKEN)
@@ -26,30 +26,12 @@ conn.close()
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    # Подключение к базе данных
-    conn = sqlite3.connect('user_data.db')
-    cursor = conn.cursor()
-
-    # Проверка, есть ли пользователь в базе данных
-    cursor.execute('SELECT * FROM users WHERE user_id = ?', (message.from_user.id,))
-    existing_user = cursor.fetchone()
-
-    # Если пользователя нет, добавляем его в базу данных с пустыми значениями
-    if not existing_user:
-        cursor.execute('INSERT INTO users (user_id, genre, gender, universe) VALUES (?, NULL, NULL, NULL)', (message.from_user.id,))
-        conn.commit()
-
-    # Закрываем соединение с базой данных
-    conn.close()
-
     # Отправляем приветственное сообщение и кнопки для выбора жанра, пола героя и вселенной
     bot.send_message(message.chat.id, 'Привет! Для начала работы выберите жанр:',
                      reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
-                                                                    one_time_keyboard=True,
-                                                                    keyboard=[
-                                                                        ['Фантастика', 'Фэнтези'],
-                                                                        ['Детектив', 'Романтика']
-                                                                    ]))
+                                                                    one_time_keyboard=True)
+                                    .add('Фантастика', 'Фэнтези')
+                                    .add('Детектив', 'Романтика'))
 
 # Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
@@ -66,16 +48,14 @@ def handle_text(message):
     cursor.execute('UPDATE users SET genre = ? WHERE user_id = ?', (genre, user_id))
     conn.commit()
 
-    # Закрываем соединение с базой данных
-    conn.close()
-
     # Отправляем сообщение с предложением выбрать пол главного героя
     bot.send_message(message.chat.id, 'Отлично! Теперь выберите пол главного героя:',
                      reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
-                                                                    one_time_keyboard=True,
-                                                                    keyboard=[
-                                                                        ['Мужской', 'Женский']
-                                                                    ]))
+                                                                    one_time_keyboard=True)
+                                    .add('Мужской', 'Женский'))
+
+    # Закрываем соединение с базой данных
+    conn.close()
 
 # Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
@@ -92,16 +72,14 @@ def handle_text(message):
     cursor.execute('UPDATE users SET gender = ? WHERE user_id = ?', (gender, user_id))
     conn.commit()
 
-    # Закрываем соединение с базой данных
-    conn.close()
-
     # Отправляем сообщение с предложением выбрать вселенную
     bot.send_message(message.chat.id, 'Отлично! Теперь выберите вселенную:',
                      reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
-                                                                    one_time_keyboard=True,
-                                                                    keyboard=[
-                                                                        ['Вселенная 1', 'Вселенная 2']
-                                                                    ]))
+                                                                    one_time_keyboard=True)
+                                    .add('Вселенная 1', 'Вселенная 2'))
+
+    # Закрываем соединение с базой данных
+    conn.close()
 
 # Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
@@ -118,11 +96,11 @@ def handle_text(message):
     cursor.execute('UPDATE users SET universe = ? WHERE user_id = ?', (universe, user_id))
     conn.commit()
 
-    # Закрываем соединение с базой данных
-    conn.close()
-
     # Отправляем сообщение с подтверждением выбора
     bot.send_message(message.chat.id, 'Отлично! Ваши предпочтения сохранены.')
+
+    # Закрываем соединение с базой данных
+    conn.close()
 
 # Запуск бота
 bot.polling()
