45ba83b158f733197705dc1abc319d53f62bf26b
Chr1stmasBread <93283740+Chr1stmasBread@users.noreply.github.com>
2024-03-20 15:47:48 +0300

    Update TOKEN.py

--- TOKEN.py
+++ TOKEN.py
@@ -1,4 +1,4 @@
 from my_token import *
 IAM_TOKEN = f'{iam_token}'
 FOLDER_ID = 'b1gh7qec08g3hugo7d7g'
-TOKEN = '6978538412:AAGd5Qo7T_meLMrnrPjLSzC_fFhAuiE7a58'
+TOKEN = '7155641386:AAGmOR8jALuKkTzSnQbYtuQY3wyRko2Nvf4'
