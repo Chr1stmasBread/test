a45e81b93e5c3183b4d03508b77dc116ece1bcc4
Chr1stmasBread <93283740+Chr1stmasBread@users.noreply.github.com>
2024-03-20 15:12:33 +0300

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -1,34 +1,35 @@
-import requests
 import telebot
-from langdetect import detect  # Импортируем функцию для определения языка
+import requests
 from TOKEN import *
-import sqlite3
 
 # Создание экземпляра бота
 bot = telebot.TeleBot(TOKEN)
 
-# Подключение к базе данных SQLite и создание курсора
-conn = sqlite3.connect('users.db')
-cursor = conn.cursor()
+# Список доступных жанров
+genres = ['Хоррор', 'Фантастика', 'Детектив', 'Романтика']
 
+# Список доступных полов главного героя
+genders = ['Мужской', 'Женский']
+
+# Список доступных вселенных
+universes = ['Средиземье', 'Галактика', 'Земля', 'Мир фэнтези']
+
+# Словарь для хранения выбранных пользователем параметров
+user_choices = {}
 
 # Функция для отправки запроса к Yandex GPT
-def generate_text(query, genre, gender, universe):
-    # Определяем URI модели на основе выбранных параметров
-    if detect(query) == 'ru':
-        model_uri = f"gpt://{FOLDER_ID}/yandexgpt-lite"
-    else:
-        model_uri = f"gpt://{FOLDER_ID}/yandexgpt-lite-en"
-
+def generate_story(user_choices, description):
     headers = {
         'Authorization': f'Bearer {IAM_TOKEN}',
         'Content-Type': 'application/json'
     }
+    # Строим текст запроса на основе выбора пользователя и описания
+    query = f"Жанр: {user_choices['genre']}\nПол главного героя: {user_choices['gender']}\nВселенная: {user_choices['universe']}\nОписание: {description}"
     data = {
-        "modelUri": model_uri,
+        "modelUri": f"gpt://{FOLDER_ID}/yandexgpt/latest",
         "completionOptions": {
             "stream": False,
-            "temperature": 0.6,
+            "temperature": 0.8,
             "maxTokens": "2000"
         },
         "messages": [
@@ -38,64 +39,62 @@ def generate_text(query, genre, gender, universe):
             }
         ]
     }
-
     response = requests.post("https://llm.api.cloud.yandex.net/foundationModels/v1/completion",
                              headers=headers,
                              json=data)
-
     if response.status_code == 200:
-        text = response.json()["result"]["alternatives"][0]["message"]["text"]
-        return text
+        story = response.json()["result"]["alternatives"][0]["message"]["text"]
+        return story
     else:
         return 'Ошибка при обработке запроса.'
-
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Привет! Для начала работы введите /generate для генерации истории.')
 
-
 # Обработчик команды /generate
 @bot.message_handler(commands=['generate'])
 def generate(message):
-    user_id = message.from_user.id
-    conn = sqlite3.connect('users.db')  # Подключение к базе данных
-    cursor = conn.cursor()
-    user_data = cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,)).fetchone()
-
-    if not user_data:
-        # Добавляем нового пользователя в базу данных
-        cursor.execute('INSERT INTO users (user_id) VALUES (?)', (user_id,))
-        conn.commit()
-        bot.reply_to(message, 'Данные пользователя успешно добавлены.')
-
-    # Создаем клавиатуру для выбора жанра и пола
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add('Фантастика', 'Фэнтези')
-    markup.add('Мужской', 'Женский')
-    markup.add('Вселенная 1', 'Вселенная 2')
-    bot.reply_to(message, 'Выберите жанр и пол главного героя:', reply_markup=markup)
+    for genre in genres:
+        markup.add(telebot.types.KeyboardButton(genre))
+    bot.send_message(message.chat.id, 'Выберите жанр:', reply_markup=markup)
 
-    conn.close()
+# Обработчик текстовых сообщений с выбором жанра
+@bot.message_handler(func=lambda message: message.text in genres)
+def choose_genre(message):
+    genre = message.text
+    user_choices['genre'] = genre
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for gender in genders:
+        markup.add(telebot.types.KeyboardButton(gender))
+    bot.send_message(message.chat.id, 'Выберите пол главного героя:', reply_markup=markup)
 
-# Обработчик текстовых сообщений
+# Обработчик текстовых сообщений с выбором пола главного героя
+@bot.message_handler(func=lambda message: message.text in genders)
+def choose_gender(message):
+    gender = message.text
+    user_choices['gender'] = gender
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for universe in universes:
+        markup.add(telebot.types.KeyboardButton(universe))
+    bot.send_message(message.chat.id, 'Выберите вселенную:', reply_markup=markup)
+
+# Обработчик текстовых сообщений с выбором вселенной
+@bot.message_handler(func=lambda message: message.text in universes)
+def choose_universe(message):
+    universe = message.text
+    user_choices['universe'] = universe
+    bot.send_message(message.chat.id, 'Введите описание для истории:')
+
+# Обработчик текстовых сообщений для генерации истории
 @bot.message_handler(func=lambda message: True)
 def handle_text(message):
-	user_id = message.from_user.id
-
-	# Подключаемся к базе данных и создаем курсор внутри этого потока
-	with sqlite3.connect('users.db') as conn:
-		cursor = conn.cursor()
-		user_data = cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,)).fetchone()
-		if not user_data:
-			bot.reply_to(message, 'Не удалось получить данные пользователя.')
-			return
-
-	query = message.text
-	generated_text = generate_text(query, user_data[4], user_data[5], user_data[6])
-	bot.reply_to(message, generated_text)
-
+    description = message.text
+    # Здесь вызываем функцию для генерации истории на основе выбора пользователя и описания
+    generated_story = generate_story(user_choices, description)
+    bot.reply_to(message, generated_story)
 
 # Запуск бота
 bot.polling()
